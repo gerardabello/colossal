1107eeeb211c928538ec57561b70e7bc09664f72
Gerard Abelló <gerardabello@gmail.com>
2016-02-28 02:51:00 +0100

    better voice structure

--- src/poly-synth/poly-synth.jsx
+++ src/poly-synth/poly-synth.jsx
@@ -4,33 +4,69 @@ import React from 'react';
 import Maps from '../maps.js';
 import Voice from './voice.js';
 
+var Octavian = require('octavian');
+
 let styles = {
     root:{
         background: "rgb(60, 37, 32)",
     }
 }
 
+let initialPreset = {
+    osc: {
+        osc1: {
+            shape: "sawtooth",
+            gain: 0
+        },
+        osc2: {
+            shape: "square",
+            gain: 0
+        },
+        osc3: {
+            shape: "sine",
+            gain: 1
+        }
+    },
+    envelopes: {
+        env1: {
+            a: 0.012,
+            al: 1.0,
+            d: 0.4,
+            dl: 0.9,
+            r: 1.0
+        }
+    }
+}
+
 var PolySynth = React.createClass({
     getInitialState: function() {
-        return {notes:{}};
+        return {notes:{}, preset: initialPreset};
     },
     componentWillReceiveProps: function(nextProps) {
     },
     //HANDLERS
-    handleMouseDown(note){
-        let v = new Voice(this.props.ctx, this.props.dstNode,440,0.012,1,0.001,1,1);
+    startVoice(signature){
+        let v = new Voice(this.props.ctx, this.props.dstNode,signature, this.state.preset);
         let n = this.state.notes;
-        n[note] = v;
+        n[signature] = v;
         this.setState({ notes: n});
     },
-    handleMouseUp(note){
-        this.state.notes[note].finish();
+    stopVoice(signature){
+        this.state.notes[signature].finish();
+        delete this.state.notes[signature];
     },
     //RENDER
     render: function() {
         return (
             <div style={ styles.root }>
-                <button type="button" onMouseDown={this.handleMouseDown.bind(this,"A4")} onMouseUp={this.handleMouseUp.bind(this,"A4")}>Click Me!</button>
+                <button type="button" onMouseDown={this.startVoice.bind(this,"A0")} onMouseUp={this.stopVoice.bind(this,"A0")}>Click Me!</button>
+                <button type="button" onMouseDown={this.startVoice.bind(this,"A1")} onMouseUp={this.stopVoice.bind(this,"A1")}>Click Me!</button>
+                <button type="button" onMouseDown={this.startVoice.bind(this,"A2")} onMouseUp={this.stopVoice.bind(this,"A2")}>Click Me!</button>
+                <button type="button" onMouseDown={this.startVoice.bind(this,"A3")} onMouseUp={this.stopVoice.bind(this,"A3")}>Click Me!</button>
+                <button type="button" onMouseDown={this.startVoice.bind(this,"A4")} onMouseUp={this.stopVoice.bind(this,"A4")}>Click Me!</button>
+                <button type="button" onMouseDown={this.startVoice.bind(this,"A5")} onMouseUp={this.stopVoice.bind(this,"A5")}>Click Me!</button>
+                <button type="button" onMouseDown={this.startVoice.bind(this,"A6")} onMouseUp={this.stopVoice.bind(this,"A6")}>Click Me!</button>
+                <button type="button" onMouseDown={this.startVoice.bind(this,"A7")} onMouseUp={this.stopVoice.bind(this,"A7")}>Click Me!</button>
             </div>
         )
     }
--- src/poly-synth/voice.js
+++ src/poly-synth/voice.js
@@ -1,40 +1,93 @@
+var Note = require('octavian').Note;
 
 class Voice {
-  constructor(ctx, dst, freq, attackTime, maxGain, sustainTime, sustainGain, decayTime) {
-      this.ctx = ctx;
-      let now = ctx.currentTime;
-      let osc = ctx.createOscillator();
-      osc.frequency.setValueAtTime(freq, now);
+    constructor(ctx, dst, signature, preset) {
+        this.context = ctx;
+        this.note = new Note(signature);
+        this.preset = preset;
 
-      let gain = ctx.createGain();
-      this.gain = gain;
-
-      osc.connect(gain);
-      gain.connect(dst);
-
-//Set envelope
-      now = ctx.currentTime;
-      gain.gain.cancelScheduledValues(now);
-      gain.gain.setValueAtTime(0.001, now);
-      gain.gain.exponentialRampToValueAtTime(maxGain, now + attackTime);
-      gain.gain.exponentialRampToValueAtTime(sustainGain, now + attackTime + sustainTime);
-
-      this.decayTime = decayTime
-
-      osc.start(0);
-  }
-
-  finish(){
-      let now = this.ctx.currentTime;
-      this.gain.gain.cancelScheduledValues(now);
-      this.gain.gain.exponentialRampToValueAtTime(0.001, now + this.decayTime);
-
-      setTimeout(
-        function() {
-            this.gain.disconnect();
-      }, this.decayTime*1000);
-  }
-}
+        this.createNodes(dst);
+        this.setPreset(this.preset);
+        this.startOsc(this.note);
 
 
-export default Voice;
+    }
+
+    createNodes(dst){
+        let ctx = this.context;
+
+        this.osc1 = ctx.createOscillator();
+        this.osc2 = ctx.createOscillator();
+        this.osc3 = ctx.createOscillator();
+
+        this.osc1Gain = ctx.createGain();
+        this.osc2Gain = ctx.createGain();
+        this.osc3Gain = ctx.createGain();
+
+        this.envGain = ctx.createGain();
+        this.mainGain = ctx.createGain();
+
+        this.osc1.connect(this.osc1Gain);
+        this.osc2.connect(this.osc2Gain);
+        this.osc3.connect(this.osc3Gain);
+
+        this.osc1Gain.connect(this.envGain);
+        this.osc2Gain.connect(this.envGain);
+        this.osc3Gain.connect(this.envGain);
+
+        this.envGain.connect(this.mainGain);
+
+        this.mainGain.connect(dst);
+    }
+
+    setPreset(p){
+        let ctx = this.context;
+
+        let now = ctx.currentTime;
+
+        //osc
+        this.osc1.type = p.osc.osc1.shape;
+        this.osc1Gain.gain.setValueAtTime(p.osc.osc1.gain, now);
+
+        this.osc2.type = p.osc.osc2.shape;
+        this.osc2Gain.gain.setValueAtTime(p.osc.osc2.gain, now);
+
+        this.osc3.type = p.osc.osc3.shape;
+        this.osc3Gain.gain.setValueAtTime(p.osc.osc3.gain, now);
+
+        //env1
+        this.envGain.gain.cancelScheduledValues(now);
+        this.envGain.gain.setValueAtTime(0.001, now);
+        this.envGain.gain.exponentialRampToValueAtTime(p.envelopes.env1.al, now + p.envelopes.env1.a);
+        this.envGain.gain.exponentialRampToValueAtTime(p.envelopes.env1.dl, now + p.envelopes.env1.a + p.envelopes.env1.d);
+    }
+
+    startOsc(note){
+        let ctx = this.context;
+
+        let now = ctx.currentTime;
+        this.osc1.frequency.setValueAtTime(note.frequency, now);
+        this.osc2.frequency.setValueAtTime(note.frequency, now);
+        this.osc3.frequency.setValueAtTime(note.frequency, now);
+
+        this.osc1.start(0);
+        this.osc2.start(0);
+        this.osc3.start(0);
+    }
+
+    finish(){
+        let now = this.context.currentTime;
+        this.mainGain.gain.cancelScheduledValues(now);
+        this.mainGain.gain.setValueAtTime(this.mainGain.gain.value, now);
+        this.mainGain.gain.exponentialRampToValueAtTime(0.001, now + this.preset.envelopes.env1.r);
+
+        setTimeout(
+            function() {
+                //avoid memory leak
+                this.mainGain.disconnect();
+            }.bind(this), 10*1000); //Todo check all envelopes for min time
+        }
+    }
+
+
+    export default Voice;
