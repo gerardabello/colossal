9d689141702113eb6b3867d7c94d8797bd5515b6
Gerard Abelló <gerardabello@gmail.com>
2016-03-02 00:33:06 +0100

    fixed gain as pow law

--- src/poly-synth/poly-synth.jsx
+++ src/poly-synth/poly-synth.jsx
@@ -78,7 +78,7 @@ var PolySynth = React.createClass({
                     <div id="osc3" className="section">
                         <h2>OSC3</h2>
                         <div className="knob-label-container">
-                            <Knob min={0} max={1} law="log" valueLink={Binder.bindToState(this,"preset", "osc.osc3.gain")}/>
+                            <Knob min={0} max={1} law="pow" valueLink={Binder.bindToState(this,"preset", "osc.osc3.gain")}/>
                             <span>Gain</span>
                         </div>
                     </div>
