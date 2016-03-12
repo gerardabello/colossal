64908b3d7e7405557526156974ddaadbff284a22
Gerard Abelló <gerardabello@gmail.com>
2016-03-12 14:07:43 +0100

    added key animation with keyboard presses

--- src/keyboard/keyboard.jsx
+++ src/keyboard/keyboard.jsx
@@ -36,6 +36,7 @@ var Keyboard = React.createClass({
         if (index > -1) {
             this.props.noteOff(signature);
             this.state.pressedKeys.splice(index, 1);
+            this.setState({pressedKeys: this.state.pressedKeys});
         }
     },
     noteOn(signature){
@@ -43,6 +44,7 @@ var Keyboard = React.createClass({
         if (index == -1) {
             this.props.noteOn(signature);
             this.state.pressedKeys.push(signature);
+            this.setState({pressedKeys: this.state.pressedKeys});
         }
     },
     //RENDER
@@ -52,15 +54,11 @@ var Keyboard = React.createClass({
 
         let nkeys = [];
         for (var i=0; i < octaves*12; i++) {
-            if(note.modifier == null){
-                nkeys.push(
-                    <div className="key white" key={note.signature} type="button" onMouseDown={this.noteOn.bind(this, note.signature)} onMouseUp={this.noteOff.bind(this, note.signature) } onMouseLeave={this.noteOff.bind(this, note.signature)}>{note.signature}</div>
-                );
-            }else{
-                nkeys.push(
-                    <div className="key black" key={note.signature} type="button" onMouseDown={this.noteOn.bind(this, note.signature)} onMouseUp={this.noteOff.bind(this, note.signature) } onMouseLeave={this.noteOff.bind(this, note.signature)}>{note.signature}</div>
-                );
-            }
+            let pressed = this.state.pressedKeys.indexOf(note.signature) != -1;
+            let black = note.modifier != null;
+            nkeys.push(
+                <div className={'key' + ' ' + (pressed? 'pressed': '') + ' ' + (black? 'black':'white')} key={note.signature} type="button" onMouseDown={this.noteOn.bind(this, note.signature)} onMouseUp={this.noteOff.bind(this, note.signature) } onMouseLeave={this.noteOff.bind(this, note.signature)}>{note.signature}</div>
+            );
             note = note.minorSecond();
         }
 
