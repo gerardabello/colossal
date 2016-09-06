554c4de08ebb69dc915c970615feb214bc12c3ce
Gerard Abelló <gerard.abello@davantis.com>
2016-09-06 16:03:39 +0200

    fixed prod build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -45,7 +45,6 @@ module.exports = {
           'NODE_ENV': JSON.stringify('production')
         }
       }),
-      new webpack.optimize.CommonsChunkPlugin('common.js'),
       new webpack.optimize.DedupePlugin(),
       new webpack.optimize.UglifyJsPlugin(),
       new webpack.optimize.AggressiveMergingPlugin()
