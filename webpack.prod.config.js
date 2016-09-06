14cc11c7dcf410ef3c0836646de2f620a53534ec
Gerard Abelló <gerard.abello@davantis.com>
2016-09-06 15:49:48 +0200

    better production build

---
+++ webpack.prod.config.js
@@ -0,0 +1,53 @@
+var webpack = require('webpack');
+
+var path = require('path');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: './main.jsx',
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build'),
+        publicPath: 'http://localhost:8080/build/'
+    },
+
+    module: {
+
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel', // 'babel-loader' is also a legal name to reference
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel', // 'babel-loader' is also a legal name to reference
+                query: {
+                    presets: ['es2015']
+                }
+            },
+
+            { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' },
+        ]
+    },
+    node: {
+        fs: 'empty'
+    },
+    plugins:[
+      new webpack.DefinePlugin({
+        'process.env':{
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.CommonsChunkPlugin('common.js'),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin(),
+      new webpack.optimize.AggressiveMergingPlugin()
+    ]
+};
